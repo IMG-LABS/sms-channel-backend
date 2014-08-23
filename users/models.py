21070f96d676235f475fc5ee49d48756df64019e
Abhas Mittal <mittalabhas1@gmail.com>
2014-08-23 10:37:11 +0530

    fix(users): added correct model file

--- users/models.py
+++ users/models.py
@@ -54,15 +54,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class UserDetails(models.Model):
 	"""
-	This class contains all the personal and public information of the user ie name, email address, phone no, dob, hometown, etc
+	This class contains all the personal and public information of the user ie name, email address, phone no, department, etc
 	"""
 	uid = models.ForeignKey(User)
 	name = models.CharField(max_length=50)
-	course = models.CharField(max_length=100)
+	department = models.CharField(max_length=100)
 	email = models.EmailField(max_length=254)
-	phoneNo = models.IntegerField(max_length=15)
-	dob = models.DateField(auto_now=False, auto_now_add=False)
-	hometown = models.CharField(max_length=50)
+	phone_number = models.IntegerField(max_length=15)
+	priviledge = models.TextField()
 
 	def __unicode__(self):
 		return self.name
