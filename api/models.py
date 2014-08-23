11f4aa3e9b25485d615cba1edbbe5cb55e4f7691
Abhas Mittal <mittalabhas1@gmail.com>
2014-08-23 10:37:53 +0530

    feat(api): added phonebook model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class PhoneBook(models.Model):
+	uid = models.ForeignKey('user', null=False, blank=False)
+	name = models.CharField(max_length=40, null=False, blank=False)
+	phone_number = models.IntegerField(null=False, blank=False)
+
+	unique_together = (("uid", "phone_number"),)
+
+	def __unicode__(self):
+		return self.name
