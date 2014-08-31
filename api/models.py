2aed94ac6e3f65857b4cf5ba54e31500df97ecf5
Abhas Mittal <mittalabhas1@gmail.com>
2014-08-31 21:46:53 +0530

    refactor(api): minor changes

--- api/models.py
+++ api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class PhoneBook(models.Model):
-	uid = models.ForeignKey('user', null=False, blank=False)
 	name = models.CharField(max_length=40, null=False, blank=False)
 	phone_number = models.IntegerField(null=False, blank=False)
+	added_by = models.ForeignKey('user', null=False, blank=False)
 
-	unique_together = (("uid", "phone_number"),)
+	# unique_together = (("added_by", "phone_number"),)
 
 	def __unicode__(self):
 		return self.name
