377485479fd5242905aabe73998f8e9693f2b9a1
Abhas Mittal <mittalabhas1@gmail.com>
2014-08-31 22:12:22 +0530

    feat(api_: added userpriviledge, phonebookgroup, smslog models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
+class UserPriviledges(models.Model):
+	"""
+	Contains the user priviledges (e.g.: HOD, Hobbies Club OC, etc)
+	"""
+	code = models.CharField(max_field=40, null=False, blank=False)
+	description = models.TextField()
+
+	def __unicode__(self):
+		return self.code
+
+class PhoneBookGroup(models.Model):
+	"""
+	Contains the segment into which phone book is divided
+	"""
+	name = models.CharField(max_length=40, null=False, blank=False)
+
+	def __unicode__(self):
+		return self.name
+
 class PhoneBook(models.Model):
+	"""
+	Contact database
+	"""
 	name = models.CharField(max_length=40, null=False, blank=False)
 	phone_number = models.IntegerField(null=False, blank=False)
 	added_by = models.ForeignKey('user', null=False, blank=False)
@@ -9,3 +31,19 @@ class PhoneBook(models.Model):
 
 	def __unicode__(self):
 		return self.name
+
+class SMSLog(models.Model):
+	"""
+	Log of the sent SMSs
+	"""
+	SUCCESS_MSG = (
+		('0', 'SUCCESS'),
+		('1', 'FAILED')
+	)
+	user = models.ForeignKey('user', null=False, blank=False)
+	sent_to = models.ForeignKey('phoneBookGroup', null=False, blank=False)
+	sent_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+	success = models.IntegerField(null=False, blank=False, choices=SUCCESS_MSG)
+
+	def __unicode__(self):
+		return self.user + " - " self.sent_to
